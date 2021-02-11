3df443da33c5a24185809488e63520ae57b3f0f4
gabyzumarraga <55769650+gabyzumarraga@users.noreply.github.com>
2021-02-11 13:00:16 -0500

    Update Hello_World.java

--- Hello_World/src/hello_world/Hello_World.java
+++ Hello_World/src/hello_world/Hello_World.java
@@ -1,8 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Hello World
+ * Displays "Hello World" text when pushing a button
  */
+
 package hello_world;
 
 import javafx.application.Application;
